76f1fabe83c49c19c049da099246f36f124fe7a4
Deuce Richards <deuce.richards.iii@gmail.com>
2016-02-21 08:00:05 -0600

    NPM autoinstaller attached

--- app/component.js
+++ app/component.js
@@ -1,7 +1,7 @@
 module.exports = function(){
 	var element = document.createElement('h1');
 
-	element.innerHTML = 'My sex is on fiyar';
+	element.innerHTML = 'Hello world';
 
 	return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecylce_event;
 
@@ -17,11 +18,25 @@ const common = {
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				// Test expacts a regexpression right here, so note the slashes
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				// Include accepts either a path or an array of paths.
+				include: PATHS.app 
+			}
+		]
 	}
 };
 
+// Default Configuration
 if(TARGET === 'start' || !TARGET){
 	module.exports = merge(common, {
+		// SOURCE MAPS IS BAD FOR PROUDCTION MMKAY
+		devtool: 'eval-source-map',
 		devServer: {
 			contentBase: PATHS.build,
 			// enable api fallback so html5 history routing works
@@ -39,7 +54,10 @@ if(TARGET === 'start' || !TARGET){
 			port: process.env.PORT
 		},
 		plugins: [
-			new webpack.HotModuleReplacementPlugin()
+			new webpack.HotModuleReplacementPlugin(),
+			new NpmInstallPlugin({
+				save: true // --save on cli
+			})
 		]
 	});
 }
