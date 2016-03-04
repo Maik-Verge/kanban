acd6a9af8ac7d4af5cddeb7b52f3e2820bf91dc4
Michael James <maik.verge@gmail.com>
2016-03-04 08:45:01 -0600

    Flux Flow + Store working

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,28 +1,29 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Change diaper'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Eat Taco Bell'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Profit'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
   }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    // Without a property intitializer 'this' wouldn't
+    // point at the right context because it defaults to
+    // 'undefined' in strict mode.
+    this.setState(state);
+  }
+
   render() {
     const notes = this.state.notes;
 
@@ -35,36 +36,22 @@ export default class App extends React.Component {
       </div>
     );
   }
-  deleteNote = (id, e) => {
-    // Avoid bubbling to edit
+  deleteNote (id, e) {
     e.stopPropagation();
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
-  };
-  editNote = (id, task) => {
-    // Don't modify if trying set an empty value
+    NoteActions.delete(id);
+  }
+
+  addNote() {
+    NoteActions.create({task: 'New Task'});
+  }
+
+  editNote (id, task) {
+    // Don't modify if trying to set an empty value
     if(!task.trim()) {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-
-      return note;
-    });
-
-    this.setState({notes});
-  };
+    NoteActions.update({id, task});
+  }
 }
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -12,7 +12,7 @@ class NoteStore {
   create(note) {
     const notes = this.notes;
 
-    node.id = uuid.v4();
+    note.id = uuid.v4();
 
     this.setState({
       notes: notes.concat(note)
@@ -21,13 +21,37 @@ class NoteStore {
 
   update(updatedNote) {
 
+    const notes = this.notes.map(note => {
+      if(note.id === updatedNote.id) {
+        /* Objec.assign is used to patch the note data here. It
+        mutates target (first parameter). In order to avoid that, use {} as its target
+        and apply data on it.
+
+        Example: {}, {a: 5, b: 3}, {a: 17} -> {a: 17, b:3}
+
+        You can pass as many objects to the method as you want. */
+
+        return Object.assign({}, note, updatedNote);
+      }
+
+      return note;
+    });
+
+    // This is the same as 'this.setState({notes: notes})'
+    //{notes} is known as a an ES6 feature known as property shorthand. This is equivalent to {notes: notes}.
+    this.setState({notes});
   }
 
   delete(id) {
 
+    this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
+
   }
 }
 
+export default alt.createStore(NoteStore, 'NoteStore');
 // Here, export is connecting the store with Alt using alt.createStore
 // Notestore, 'NoteStore' - ain't have to do this, but when this shit gets minified later
 // it's possible it may nuke the relation between these two. Grand practice to name them the exact
