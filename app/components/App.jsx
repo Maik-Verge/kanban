1a72f4990d76e9e2771bce3efb1696c06734cc62
Michael James <maik.verge@gmail.com>
2016-03-05 11:17:49 -0600

    Lanes mirrored, but multiple lanes are propagating

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,57 +1,71 @@
+import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
+// Getting rid of these in favor of lanes
+// import Notes from './Notes.jsx';
 
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+// import NoteActions from '../actions/NoteActions';
+// import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
+
+
+//Alt makes all of this redundant
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = NoteStore.getState();
-  }
+//     this.state = NoteStore.getState();
+//   }
 
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-  storeChanged = (state) => {
-    // Without a property intitializer 'this' wouldn't
-    // point at the right context because it defaults to
-    // 'undefined' in strict mode.
-    this.setState(state);
-  }
+//   componentDidMount() {
+//     NoteStore.listen(this.storeChanged);
+//   }
+//   componentWillUnmount() {
+//     NoteStore.unlisten(this.storeChanged);
+//   }
+//   storeChanged = (state) => {
+//     // Without a property intitializer 'this' wouldn't
+//     // point at the right context because it defaults to
+//     // 'undefined' in strict mode.
+//     this.setState(state);
+//   }
 
   render() {
-    const notes = this.state.notes;
+    // const notes = this.state.notes;
 
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
+        <button className="add-note" onClick={this.addLane}>+</button>
+        <AltContainer
+          stores={[LaneStore]}
+          inject={{
+            lanes: () => LaneStore.getState().lanes || []
+          }}
+        >
+          <Lanes />
+        </AltContainer>
       </div>
     );
   }
-  deleteNote (id, e) {
-    e.stopPropagation();
+  // addNote() {
+  //   NoteActions.create({task: 'New Task'});
+  // }
+  // editNote (id, task) {
+  //   // Don't modify if trying to set an empty value
+  //   if(!task.trim()) {
+  //     return;
+  //   }
 
-    NoteActions.delete(id);
-  }
+  //   NoteActions.update({id, task}) ;
+  // }
+  // deleteNote (id, e) {
+  //   e.stopPropagation();
 
-  addNote() {
-    NoteActions.create({task: 'New Task'});
-  }
-
-  editNote (id, task) {
-    // Don't modify if trying to set an empty value
-    if(!task.trim()) {
-      return;
-    }
-
-    NoteActions.update({id, task});
+  //   NoteActions.delete(id);
+  // }
+  addLane() {
+    LaneActions.create({name: 'New lane'});
   }
 }
